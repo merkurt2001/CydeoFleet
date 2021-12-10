bf3614eddec1cf9cdb546332764c1552d7307a9c
merkurt2001 <merkurt2001@gmail.com>
2021-12-10 23:28:02 +0300

    dry

--- src/test/java/com/fleet/runners/CukesRunner.java
+++ src/test/java/com/fleet/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports"},
         features = "src/test/resources/features",
         glue = "com/fleet/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@TRN-1241"
 )
 public class CukesRunner {
