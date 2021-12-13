da8c809b69574fc4e21ef2096dd3f15f3be6ddf0
merkurt2001 <merkurt2001@gmail.com>
2021-12-13 22:43:23 +0300

    login via command line added

--- src/test/java/com/fleet/runners/CukesRunner.java
+++ src/test/java/com/fleet/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/fleet/step_definitions",
         dryRun = false,
-        tags = "@command"
+        tags = "@TRN-1241"
 )
 public class CukesRunner {
 }
